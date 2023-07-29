84258cfa64a4a5c933c864935bbd688917203158
Ilya <ilya.shuvatov1@gmail.com>
2023-07-29 13:34:44 +0200

    cors problem resolved

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASE_URL = "https://mernyblog-api.onrender.com";
+const BASE_URL = "https://merny-api.onrender.com";
 
 export default axios.create({
   baseURL: BASE_URL
